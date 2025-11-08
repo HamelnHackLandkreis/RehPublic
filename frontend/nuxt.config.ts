d25842d22c7e3113e46d4ce2421a4e8f277a50cf
StromFLIX <felix.moenckemeyer@gmail.com>
2025-11-08 16:09:25 +0100

    fix

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/eslint', '@nuxt/ui', '@nuxtjs/tailwindcss']
+  css: ['./app/assets/css/main.css'],
+  modules: ['@nuxt/image', '@nuxt/eslint', '@nuxt/ui'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
